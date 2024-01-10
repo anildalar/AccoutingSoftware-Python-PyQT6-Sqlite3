496d92ba067dd11bddc0ee9885a02b9b8ecd4796
anildalar <dollorinfotech@gmail.com>
2024-01-10 09:12:55 +0530

    new

--- register.py
+++ register.py
@@ -3,20 +3,41 @@ import json
 import os
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
 
-print("Current Working Directory:", os.getcwd())
-# Get the absolute path to the script's directory
-script_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Construct the absolute path to the "accounting.json" file
-json_file_path = os.path.join(script_directory, 'accounting.json')
-if os.path.exists(json_file_path):
-    with open(json_file_path, 'r') as file:
-        settings = json.load(file)
-        print(settings)
 
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle('Admin Registration')
-window.showMaximized()
-window.show()
+#1. Class defination
+#class ChildClass(ParentClass): PascalCase
+class AccountingApp(QWidget):
+    #1.Properties = Varaible = Store
+    setting={}
+    #2. Class constructor
+    def __init__(self): # self=cio
+        super().__init__() # I can call the parent construtor this way
+        print("Hello from constructor")
+        # Lets try to read the json file
+        try:
+            with open('accounting.json', 'r') as file:
+                self.settings = json.load(file)
+        except FileNotFoundError:
+            self.settings = {}
+        print(self.settings)    
+        print(type(self.settings))
+        self.buildUI()
+        pass
+    
+    #3.Method=Function
+    
+    def buildUI(self):
+        #self=window
+        self.setStyleSheet("background-color: #A4BFD8;") #aa
+        self.setWindowTitle(self.settings["windowTitle"])
+        self.showMaximized()
+        self.show()
+        pass
+    pass
+#2. Create class Object
+app = QApplication([])
+ceo = AccountingApp()
+ceo.showMaximized()
 sys.exit(app.exec())
+
