3d1a078e148b1ea0875edf796e1552d918e2f2a6
anildalar <dollorinfotech@gmail.com>
2024-01-11 09:20:43 +0530

    ne

--- register.py
+++ register.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import os
+import winreg
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
 
 
@@ -14,18 +15,57 @@ class AccountingApp(QWidget):
     def __init__(self): # self=cio
         super().__init__() # I can call the parent construtor this way
         print("Hello from constructor")
-        # Lets try to read the json file
+        #2. function calling
+        self.readJsonFile()
+        self.checkIfAdminIsCreated()
+        self.buildUI()
+        pass
+    
+    #3.Method=Function
+    def readJsonFile(self):
+         # Lets try to read the json file
         try:
             with open('accounting.json', 'r') as file:
                 self.settings = json.load(file)
         except FileNotFoundError:
             self.settings = {}
         print(self.settings)    
-        print(type(self.settings))
-        self.buildUI()
+        print(type(self.settings)) #cio
+        pass 
+    # Define
+    def checkIfAdminIsCreated(self):
+        print('Hello from checkIfAdminIsCreated')
+        
+        registry_key_path = r"SOFTWARE\accountingsoftware"
+        value_name = "isAdminCreated"
+         # Open the registry key for reading
+        try:
+            # Open the registry key for reading
+            key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, registry_key_path)
+            
+            # Read the value of the specified entry
+            value, _ = winreg.QueryValueEx(key, value_name)
+            #print(type(value.lower()))
+            if value.lower() == "true":
+                # Show the Login Form
+                print("Show the Login Form")
+                pass
+            elif value.lower() == "false":
+                # Show the Registeration Form
+                print("Show the Registeration Form")
+                pass
+            else:
+                # Create a entry with the registry key
+                # isAdminCreated= false
+                print("Create a entry isAdminCreated= false")
+                
+            # Close the registry key
+        except FileNotFoundError:
+            key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, registry_key_path)
+            winreg.SetValueEx(key, value_name, 0, winreg.REG_SZ, "false")
+        
+        winreg.CloseKey(key)
         pass
-    
-    #3.Method=Function
     
     def buildUI(self):
         #self=window
